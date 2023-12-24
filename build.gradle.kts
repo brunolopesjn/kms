7592cacc89ff6d8f11ef0ba429f36167aaf848c6
Bruno Lopes <brunolopesjn@gmail.com>
2023-12-24 06:06:50 -0300

    :sparkles: feat: Configurado o Micronaut Data JDBC e Flyway sobre o PostgreSQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,13 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.4")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.5")
 
+    // Micronaut Data JDBC, Flyway e PostgreSQL
+    ksp("io.micronaut.data:micronaut-data-processor:4.4.1")
+    implementation("io.micronaut.data:micronaut-data-jdbc:4.4.1:")
+    implementation("io.micronaut.flyway:micronaut-flyway:6.2.1")
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari:5.4.0")
+    runtimeOnly("org.postgresql:postgresql:42.7.1")
+
 }
 
 application {
